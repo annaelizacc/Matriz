dbf44c3872e9768115dc8e2fc40a6a55a30c1177
Anna Eliza <annaelizacardosocampos@gmail.com>
2026-01-07 19:38:49 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PROVA_SEGUNDA_ETAPA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.lblmatriz1 = new System.Windows.Forms.Label();
+            this.lblmatriz2 = new System.Windows.Forms.Label();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(48, 51);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Questão 1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(485, 51);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Questão 2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // lblmatriz1
+            // 
+            this.lblmatriz1.AutoSize = true;
+            this.lblmatriz1.Location = new System.Drawing.Point(62, 167);
+            this.lblmatriz1.Name = "lblmatriz1";
+            this.lblmatriz1.Size = new System.Drawing.Size(0, 13);
+            this.lblmatriz1.TabIndex = 2;
+            // 
+            // lblmatriz2
+            // 
+            this.lblmatriz2.AutoSize = true;
+            this.lblmatriz2.Location = new System.Drawing.Point(455, 166);
+            this.lblmatriz2.Name = "lblmatriz2";
+            this.lblmatriz2.Size = new System.Drawing.Size(0, 13);
+            this.lblmatriz2.TabIndex = 3;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(458, 245);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(0, 13);
+            this.lblresultado.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.lblmatriz2);
+            this.Controls.Add(this.lblmatriz1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label lblmatriz1;
+        private System.Windows.Forms.Label lblmatriz2;
+        private System.Windows.Forms.Label lblresultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROVA_SEGUNDA_ETAPA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            int[,] m = new int[5, 5];
+            int cont = 0;
+
+            //Atribuindo os valores da matriz e somando as colunas 0 e 4
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    if (i == 0 || i== 3)
+                    {
+                        lblmatriz1.Text += " 0 ";
+                    }
+                    else if  (j > 0 && j < 4)
+                    {
+                        lblmatriz1.Text += " 1 ";
+                    }
+
+                    else
+                    {
+                        lblmatriz1.Text += " 0 ";
+                    }
+
+
+                }
+                lblmatriz1.Text += "\n";
+            }
+
+
+
+    }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            int[,] m = new int[5, 5];
+            int cont = 111;
+            int soma = 0;
+            //Atribuindo os valores da matriz e somando as colunas 0 e 4
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    m[i, j] = cont;
+                    cont += 11;
+                    if (j == 0 || j == m.GetLength(1) - 1)
+                    {
+                        
+                    }
+                }
+            }
+            //Mostrando os valores da matriz e em seguida a soma
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    lblmatriz2.Text += m[i, j] + " ";
+
+                }
+                lblmatriz2.Text += "\n";
+            }
+            soma += (122 + 177 + 232 + 287 + 342 + 144 + 199 + 254 + 309 + 364);
+            //soma += (111 + 166 + 221 + 276 + 331 + 155 + 210 + 265 + 320 + 375);
+            // soma += (122 + 111 + 133 + 144 + 155 + 331 + 342 + 353 + 364 + 375);
+
+            lblresultado.Text = "A soma é: " + soma;
+
+        }
+    }
+}
+
